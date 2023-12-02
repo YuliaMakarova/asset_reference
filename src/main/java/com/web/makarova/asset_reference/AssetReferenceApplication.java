cd73aa090e99d9a3d6e016b13924805988500e0a
YuliaMakarova <juliamakarova2172@gmail.com>
2023-12-02 17:37:05 +0400

    инициализация проекта

---
+++ src/main/java/com/web/makarova/asset_reference/AssetReferenceApplication.java
@@ -0,0 +1,13 @@
+package com.web.makarova.asset_reference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetReferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetReferenceApplication.class, args);
+    }
+
+}
