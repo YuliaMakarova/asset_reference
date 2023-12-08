44a1ec24f74ebc84f5b614aaaef568d7727d35ca
YuliaMakarova <juliamakarova2172@gmail.com>
2023-12-08 16:45:22 +0400

    изменения для Леры

---
+++ target/classes/db/migration/V2__create_exchange_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE exchange (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL,
+                          code VARCHAR(10) NOT NULL
+);
